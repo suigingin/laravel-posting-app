eab41ddc3015ce3af551eec884d30a9c81e2c102
suigingin <saigyoujiyyk@gmail.com>
2023-07-17 16:31:58 +0900

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <nav class="navbar navbar-light bg-light fixed-top" style="height: 60px;">
+        <div class="container">
+            <a href="{{ route('posts.index') }}" class="navbar-brand">一覧</a> -
+            <a href="{{ route('posts.create') }}" class="navbar-brand">投稿</a>
+        </div>
+    </nav>
+</header>
